26d94e52c7584a79823c6810635ebfe125c7718e
Ronny Jimmy <ronnyrtg@hotmail.com>
2018-06-10 09:36:03 +0800

    Liquor Application update

--- TradingLicense.Entities/LAReqDoc.cs
+++ TradingLicense.Entities/LAReqDoc.cs
@@ -2,15 +2,13 @@
 
 namespace TradingLicense.Entities
 {
-    public class SALinkReqDoc
+    public class LAReqDoc
     {
         [Key]
         public int LAReqDocID { get; set; }
-        public int LiquorApplicationID { get; set; }
+
         public int RequiredDocID { get; set; }
-        public int AttachmentID { get; set; }
 
         public virtual RequiredDoc RequiredDoc { get; set; }
-        public virtual LiquorApplication LiquorApplication { get; set; }
     }
 }
