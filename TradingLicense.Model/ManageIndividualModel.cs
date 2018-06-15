1fe145363c3ebe3bd59aa4a9eb1f9678b3f5fe74
Aravind <getaravindkumar@gmail.com>
2018-06-15 17:29:19 +0530

    Issue#32 - Changed upload location in ManageIndividual

--- TradingLicense.Model/ManageIndividualModel.cs
+++ TradingLicense.Model/ManageIndividualModel.cs
@@ -10,5 +10,33 @@ namespace TradingLicense.Model
     {
         public IndividualModel Individual { get; set; }
         public string CompanyIds { get; set; }
+
+        private string _tempIndividualLoc = string.Empty;
+        public string TempIndividualLoc
+        {
+            get
+            {
+                if(_tempIndividualLoc == string.Empty)
+                {
+                    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+                    var stringChars = new char[6];
+                    var random = new Random();
+
+                    for (int i = 0; i < stringChars.Length; i++)
+                    {
+                        stringChars[i] = chars[random.Next(chars.Length)];
+                    }
+
+                    var randomString = new String(stringChars);
+                    _tempIndividualLoc = "_" + randomString;
+                }
+                return _tempIndividualLoc;
+            }
+
+            set
+            {
+                _tempIndividualLoc = value;
+            }
+        }
     }
 }
