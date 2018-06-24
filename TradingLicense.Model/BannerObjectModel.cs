e878f6ba2966003d48975c37f68e56b4a57c7d94
Ronny Jimmy <ronny@datatechnet.com>
2018-06-25 01:42:50 +0800

    Removed Zone from BannerApplication

--- TradingLicense.Model/BannerObjectModel.cs
+++ TradingLicense.Model/BannerObjectModel.cs
@@ -5,16 +5,12 @@ namespace TradingLicense.Model
     public class BannerObjectModel
     {
         public int BannerObjectID { get; set; }
-
-        [Display(Name = "Banner Location")]
+        public int BannerApplicationID { get; set; }
+        public int BQuantity { get; set; }
+        public float BSize { get; set; }
         [Required(ErrorMessage = "Sila masukkan lokasi iklan")]
-        [StringLength(5)]
-        public int LocationID { get; set; }
-        [Display(Name = "Banner Zone")]
-        [Required(ErrorMessage = "Sila masukkan lokasi zon iklan")]
         [StringLength(255)]
-        public int ZoneID { get; set; }
-        [Display(Name = "Kod Iklan")]
+        public int LocationID { get; set; }
         [Required(ErrorMessage = "Sila pilih kod iklan")]
         public int BannerCodeID { get; set; }
     }
