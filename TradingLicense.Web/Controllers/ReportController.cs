fbb056ea77ea9f2c2923f460aa6c8cd65d06d6b3
Jeffry <paragasu@gmail.com>
2018-07-21 20:17:55 -0700

    add controllers

--- TradingLicense.Web/Controllers/ReportController.cs
+++ TradingLicense.Web/Controllers/ReportController.cs
@@ -1,25 +1,14 @@
-using AutoMapper;
-using System;
-using System.Data.Entity.Migrations;
-using System.Linq;
-using System.Net;
+using System.Web;
 using System.Web.Mvc;
-using System.Web.Security;
-using TradingLicense.Data;
-using TradingLicense.Entities;
-using TradingLicense.Infrastructure;
-using TradingLicense.Model;
-using TradingLicense.Web.Pages;
-using static TradingLicense.Infrastructure.Enums;
 
 namespace TradingLicense.Web.Controllers
 {
     public class ReportController : Controller
     {
-
-        public ActionResult Index()
+        // GET /Report
+        public string Index()
         {
-            return View();
+            return "Hello World!";
         }
     }
 }
