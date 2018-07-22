2b5a997b9de250bcf922b4bc609b8e943c32817b
Jeffry <paragasu@gmail.com>
2018-07-21 20:37:46 -0700

    update report controller

--- TradingLicense.Web/Controllers/ReportController.cs
+++ TradingLicense.Web/Controllers/ReportController.cs
@@ -6,9 +6,129 @@ namespace TradingLicense.Web.Controllers
     public class ReportController : Controller
     {
         // GET /Report
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello World!";
+            return View();
+        }
+
+        public ActionResult LicenseBusinessTypeMaster()
+        {
+            return View();
+        }
+
+        public ActionResult LicenceTypeLookup()
+        {
+            return View();
+        }
+
+        public ActionResult ZoneMaster()
+        {
+            return View();
+        }
+
+        public ActionResult SubzoneMaster()
+        {
+            return View();
+        }
+
+        public ActionResult RoadMaster()
+        {
+            return View();
+        }
+
+        public ActionResult CitizenMaster()
+        {
+            return View();
+        }
+
+        public ActionResult CitizenLookup()
+        {
+            return View();
+        }
+
+        public ActionResult RaceMaster()
+        {
+            return View();
+        }
+
+        public ActionResult BankMaster()
+        {
+            return View();
+        }
+
+        public ActionResult OwnerMaster()
+        {
+            return View();
+        }
+
+        public ActionResult JointOwner()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentTransaction()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentCollection()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentInquiry()
+        {
+            return View();
+        }
+
+        public ActionResult AdjustmentListing()
+        {
+            return View();
+        }
+
+        public ActionResult Duration()
+        {
+            return View();
+        }
+
+        public ActionResult LicenseListing()
+        {
+            return View();
+        }
+
+        public ActionResult OwnerInfo()
+        {
+            return View();
+        }
+
+        public ActionResult License()
+        {
+            return View();
+        }
+
+        public ActionResult LicenseApplicationStats()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationStatus()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationLocationStats()
+        {
+            return View();
+        }
+
+        public ActionResult RenewalStatusResult()
+        {
+            return View();
+        }
+
+        public ActionResult BusinessListing()
+        {
+            return View();
         }
     }
 }
