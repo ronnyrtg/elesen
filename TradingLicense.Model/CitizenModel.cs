e80bce94923e5a1d9dd206eee9de0944a3cc4dab
Jeffry <paragasu@gmail.com>
2018-07-31 15:04:14 -0700

    add automapper for citizen

--- TradingLicense.Model/CitizenModel.cs
+++ TradingLicense.Model/CitizenModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TradingLicense.Model
 {
-    class CitizenModel
+    public class CitizenModel
     {
         [Key]
         public int CITIZENID { get; set; }
