9a6e2c01835c32326130ab7f5e8ac012399f85bd
Jeffry <paragasu@gmail.com>
2018-07-31 14:56:11 -0700

    fix zone column name

--- TradingLicense.Model/ZoneModel.cs
+++ TradingLicense.Model/ZoneModel.cs
@@ -13,8 +13,7 @@ namespace TradingLicense.Model
         [Display(Name = "Subzone Description")]
         [Required(ErrorMessage = "Please enter Zone Description")]
         [StringLength(255)]
-        public string SUBZONE_CODE { get; set; }
-        public string SUBZONE_DESC { get; set; }
+        public string ZONE_DESC { get; set; }
         [Display(Name = "Is Active")]
         public bool ACTIVE { get; set; }
         public List<Select2ListItem> zoneCombineList = new List<Select2ListItem>();
