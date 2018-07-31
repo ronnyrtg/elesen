1f45fb96e6737cfa4b39fbac32ae84a1c129b7e7
Jeffry <paragasu@gmail.com>
2018-07-31 15:02:06 -0700

    add citizen model

--- TradingLicense.Model/CitizenModel.cs
+++ TradingLicense.Model/CitizenModel.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace TradingLicense.Entities
+namespace TradingLicense.Model
 {
-    public class CITIZEN_M
+    class CitizenModel
     {
-
         [Key]
         public int CITIZENID { get; set; }
         [StringLength(10)]
